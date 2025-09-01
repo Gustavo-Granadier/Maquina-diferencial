e50c596e257b77f49d3447e2032a3dcc05528b6d
Gustavo-Granadier <139660911+Gustavo-Granadier@users.noreply.github.com>
2025-09-01 11:14:36 -0300

    Update babage80.py

--- babage80.py
+++ babage80.py
@@ -23,9 +23,9 @@ def maquina_diferencial(tabela_diferencas):
     tabela_extrapolada = [list(row) for row in tabela_diferencas]
 
     for i in range(grau - 1, -1, -1):
-        proximo_valor_linha_abaixo = tabela_extrapolada[i+1][-1]
-        ultimo_valor_linha_atual = tabela_extrapolada[i][-1]
-        novo_valor_linha_atual = ultimo_valor_linha_atual + proximo_valor_linha_abaixo
+        valor_linha_abaixo = tabela_extrapolada[i+1][-1]
+        valor_linha_atual = tabela_extrapolada[i][-1]
+        novo_valor_linha_atual = valor_linha_atual + valor_linha_abaixo
         tabela_extrapolada[i].append(novo_valor_linha_atual)
 
     return tabela_extrapolada
@@ -35,17 +35,17 @@ coeficientes = [9, -3, 2, 9, 2]
 eixo_x = list(range(9))
 eixo_y = [polinomio(x, coeficientes) for x in eixo_x]
 
-diffs = babbage(eixo_y, len(coeficientes) - 1)
+tabela_babage = babbage(eixo_y, len(coeficientes) - 1)
 
 print("Valores de x:", eixo_x)
 print("Valores de y:", eixo_y)
 print("\nTabela de Diferencas:")
-for i, diff in enumerate(diffs):
+for i, diff in enumerate(tabela_babage):
     print(f"Diferenca {i}: {diff}")
 
 proximo_x = eixo_x[-1] + (eixo_x[1] - eixo_x[0])
 
-tabela_nova = maquina_diferencial(diffs)
+tabela_nova = maquina_diferencial(tabela_babage)
 
 print("\nTabela de Diferencas (atualizada):")
 for i, linha in enumerate(tabela_nova):
